ca6a4eb036294c0487f9ea1189fbd6c6dd16751a
Vahid <vnoroozi@nvidia.com>
2021-06-16 13:31:41 -0700

    added jit helpers.
    Signed-off-by: Vahid <vnoroozi@nvidia.com>

--- nemo/collections/asr/modules/conformer_encoder.py
+++ nemo/collections/asr/modules/conformer_encoder.py
@@ -221,10 +221,10 @@ class ConformerEncoder(NeuralModule, Exportable):
         bs, xmax, idim = audio_signal.size()
 
         # Create the self-attention and padding masks
-        #pad_mask = self.make_pad_mask(length, max_time=xmax, device=audio_signal.device)
-        #att_mask = pad_mask.unsqueeze(1).repeat([1, xmax, 1])
-        #att_mask = att_mask & att_mask.transpose(1, 2)
-        pad_mask, att_mask = ConformerEncoder.slice_helper_make_pad_mask(seq_lens=length, audio_signal=audio_signal)
+        pad_mask = self.make_pad_mask(length, max_time=xmax, device=audio_signal.device)
+        att_mask = pad_mask.unsqueeze(1).repeat([1, xmax, 1])
+        att_mask = att_mask & att_mask.transpose(1, 2)
+        #pad_mask, att_mask = ConformerEncoder.slice_helper_make_pad_mask(seq_lens=length, audio_signal=audio_signal)
 
         if self.att_context_size[0] >= 0:
             att_mask = att_mask.triu(diagonal=-self.att_context_size[0])
